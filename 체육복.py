bf36ea34d086490293a899b27fa6de7f43800963
Shin-GC <tlsrhkdcjs12@naver.com>
2021-01-14 10:41:36 +0900

    Greedy Coding test Practice

---
+++ 체육복.py
@@ -0,0 +1,23 @@
+def solution(n, lost, reserve):
+    answer =len(reserve)
+    n-=len(set(lost+reserve))
+    answer+=n
+    
+    for l in lost[:]:
+        for r in reserve:
+            if l==r:
+                reserve.remove(r)
+                lost.remove(l)
+    for l in lost[:]:
+        if l-1 in reserve:
+            answer+=1
+            reserve.remove(l-1)
+            lost.remove(l)
+            continue
+        elif l+1 in reserve:
+            answer+=1
+            reserve.remove(l+1)
+            lost.remove(l)
+            continue
+    
+    return answer
