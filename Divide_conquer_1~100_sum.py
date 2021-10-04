7c9948c52eb4215b946f000497f335bad928e689
ShinGwangCheon <58453569+Shin-GC@users.noreply.github.com>
2021-10-04 14:01:36 +0900

    분할정복 알고리즘

--- Divide_conquer_1~100_sum.py
+++ Divide_conquer_1~100_sum.py
@@ -1,11 +1,10 @@
 def consecutive_sum(start, end):
 
     if start == end:
-        print(f"return : {start}")
         return start
 
     mid = (start + end) // 2
-    print(f"start : {start}, mid: {mid}, end: {end}")
+
     return consecutive_sum(start, mid) + consecutive_sum(mid + 1, end)
 
 
